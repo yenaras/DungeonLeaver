e17759a8b62ab8d1645945d91ff68cb7f491015c
yenaras <brandon82890@gmail.com>
2023-11-27 13:47:18 -0500

    initial commit

---
+++ DungeonLeaver.lua
@@ -0,0 +1,24 @@
+DungeonLeaver = {}
+
+DungeonLeaver.name = "DungeonLeaver"
+
+function DungeonLeaver.OnAddonLoaded(event, addonName)
+    if addonName ~= DungeonLeaver.name then return end
+    EVENT_MANAGER:UnregisterForEvent(DungeonLeaver.name, EVENT_ADD_ON_LOADED)
+end
+
+local function LeaveDungeon()
+    if IsUnitGrouped("player") then
+        GroupLeave()
+    end
+    if CanExitInstanceImmediately() then
+        ExitInstanceImmediately()
+    end
+end
+
+SLASH_COMMANDS["/dl"] = LeaveDungeon
+SLASH_COMMANDS["/dungeonleave"] = LeaveDungeon
+SLASH_COMMANDS["/ld"] = LeaveDungeon
+SLASH_COMMANDS["/leavedungeon"] = LeaveDungeon
+
+EVENT_MANAGER:RegisterForEvent(DungeonLeaver.name, EVENT_ADD_ON_LOADED, DungeonLeaver.OnAddOnLoaded)
