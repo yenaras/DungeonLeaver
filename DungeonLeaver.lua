98c45ab97a3092a2b323120724fc0b9aa93a2931
yenaras <brandon82890@gmail.com>
2023-12-03 00:48:49 -0500

    add 1 second delay between leaving group and dungeon

--- DungeonLeaver.lua
+++ DungeonLeaver.lua
@@ -1,30 +1,37 @@
 DungeonLeaver = {}
-
 DungeonLeaver.name = "DungeonLeaver"
 
-function DungeonLeaver.OnAddOnLoaded(event, addonName)
-    if addonName ~= DungeonLeaver.name then return end
-    EVENT_MANAGER:UnregisterForEvent(DungeonLeaver.name, EVENT_ADD_ON_LOADED)
+-- if player is in an instance, leave the instance
+local function InstanceLeaver()
+    if CanExitInstanceImmediately() then
+        ExitInstanceImmediately()
+        d("leaving instance")
+    end
 end
 
 local function LeaveDungeon()
     -- if player is in a group, leave the group
     if IsUnitGrouped("player") then
         GroupLeave()
-    end
-    -- if player is in an instance, leave the instance
-    if CanExitInstanceImmediately() then
-        ExitInstanceImmediately()
+        d("leaving group")
+        zo_callLater(function() InstanceLeaver() end, 1000)
+    else
+        InstanceLeaver()
     end
 end
 
-SLASH_COMMANDS["/dl"] = LeaveDungeon
-SLASH_COMMANDS["/dungeonleave"] = LeaveDungeon
-SLASH_COMMANDS["/ld"] = LeaveDungeon
-SLASH_COMMANDS["/leavedungeon"] = LeaveDungeon
-SLASH_COMMANDS["/gl"] = LeaveDungeon
-SLASH_COMMANDS["/groupleave"] = LeaveDungeon
-SLASH_COMMANDS["/lg"] = LeaveDungeon
-SLASH_COMMANDS["/leavegroup"] = LeaveDungeon
+function DungeonLeaver.OnAddOnLoaded(event, addonName)
+    if addonName ~= DungeonLeaver.name then return end
+    EVENT_MANAGER:UnregisterForEvent(DungeonLeaver.name, EVENT_ADD_ON_LOADED)
+
+    SLASH_COMMANDS["/dl"] = LeaveDungeon
+    SLASH_COMMANDS["/dungeonleave"] = LeaveDungeon
+    SLASH_COMMANDS["/ld"] = LeaveDungeon
+    SLASH_COMMANDS["/leavedungeon"] = LeaveDungeon
+    SLASH_COMMANDS["/gl"] = LeaveDungeon
+    SLASH_COMMANDS["/groupleave"] = LeaveDungeon
+    SLASH_COMMANDS["/lg"] = LeaveDungeon
+    SLASH_COMMANDS["/leavegroup"] = LeaveDungeon
+end
 
 EVENT_MANAGER:RegisterForEvent(DungeonLeaver.name, EVENT_ADD_ON_LOADED, DungeonLeaver.OnAddOnLoaded)
